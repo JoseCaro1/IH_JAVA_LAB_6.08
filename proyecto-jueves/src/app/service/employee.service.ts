168f70c8caa20b898e655db32647434033dadbeb
DESKTOP-LHQMMFT\JoseK <jcaroromero18@gmail.com>
2022-06-28 09:19:37 +0200

    Finish lab

---
+++ proyecto-jueves/src/app/service/employee.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http'
+import { Employee } from '../model/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http:HttpClient) { }
+
+  getEmployees() : Observable<Employees[]> {
+    return this.http.get<Employees[]>('http://localhost:8080/employee/find-all')
+  }
+
+  deleteEmployee(id:number) : Observable<void> {
+    return this.http.delete<void>(`http://localhost:8080/employee/delete/${id}`)
+  }
+
+
+}
+
+
+export interface Employees {
+  id:number,
+  name:string,
+  phoneNumber:string,
+  officeNumber:string,
+  position:string,
+  manager:string,
+
+
+}
+
+/**
+ * {results: [
+ *  {},
+ *  {}
+ * ],
+ * status:200}
+ * 
+ * result: [
+ *  {},
+ *  {}
+ * ]
+ * 
+ */
